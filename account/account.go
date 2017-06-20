47e5fcc785a3747c7f4790368d19c5e5db1f391c
ak-sahli <ak-sahli@protonmail.com>
2017-06-20 20:15:44 +0200

    Deposit and withdraw a valid amount of money

--- account/account.go
+++ account/account.go
@@ -1,5 +1,23 @@
 package account
 
 type Account struct {
+	balance float64
+	owner   string
+}
 
+func NewAccount(initialAmount float64, owner string) *Account {
+	return &Account{initialAmount, owner}
+}
+
+func (account *Account) Deposit(amount float64) {
+	account.balance += amount
+
+}
+
+func (account *Account) Withdraw(amount float64) {
+  account.balance -= amount
+}
+
+func (account Account) Balance() float64 {
+	return account.balance
 }
--- account/account_test.go
+++ account/account_test.go
@@ -1,14 +1,26 @@
 package account
 
 import (
-  "testing"
+	"testing"
 )
 
-
 func TestDepositValidAmount(t *testing.T) {
+	account := NewAccount(0.0, "Bruce Wayne")
+	account.Deposit(200.0)
+	expectedAccountBalance := 200.0
+	actualAccountBalance := account.Balance()
+	if expectedAccountBalance != actualAccountBalance {
+		t.Fatalf("Failed to deposit money in the bank account, expected: %f, actual: %f", expectedAccountBalance, actualAccountBalance)
+	}
 
 }
 
 func TestWithdrawValidAmount(t *testing.T) {
-
+  account := NewAccount(200.0, "Bruce Wayne")
+  account.Withdraw(50.00)
+  expectedAccountBalance := 150.0
+	actualAccountBalance := account.Balance()
+  if expectedAccountBalance != actualAccountBalance {
+		t.Fatalf("Failed to deposit money in the bank account, expected: %f, actual: %f", expectedAccountBalance, actualAccountBalance)
+	}
 }
